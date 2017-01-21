7662d49f475459a99a382d271b1705c2bdc111aa
Morusque <Morusque@gmail.com>
2017-01-21 21:34:41 +0100

    animated frames

---
+++ Assets/AnimatedFrames.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedFrames : MonoBehaviour {
+
+    public Sprite[] frames = new Sprite[3];
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
