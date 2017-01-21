19ab5c1e982c326c8e4ae212d9d0822d90c89a99
Morusque <Morusque@gmail.com>
2017-01-21 21:42:09 +0100

    animated backgrounds

--- Assets/AnimatedFrames.cs
+++ Assets/AnimatedFrames.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class AnimatedFrames : MonoBehaviour {
 
     public Sprite[] frames = new Sprite[3];
-    
+    public float animSpeed = 1;
+    int currentFrame = 0;
+    float cTime = 0;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,6 +16,7 @@ public class AnimatedFrames : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        cTime += Time.deltaTime;
+        GetComponent<SpriteRenderer>().sprite = frames[(Mathf.FloorToInt(cTime*animSpeed)%frames.Length)];
+    }
 }
