83eb73f37cb95c7b9deb255b632a6c9603aaf499
Morusque <Morusque@gmail.com>
2017-01-21 01:27:15 +0100

    Waveshapes

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour {
 
+    [Range(-5.0f, 5.0f)]
     public float[] surfaceHeights;
     public OneOsc[] osc = new OneOsc[1];
     
@@ -12,10 +13,12 @@ public class Oscillator : MonoBehaviour {
 	}
 	
 	void Update () {
-        for (int i=0;i<osc.Length;i++) {
-            for (int j=0 ; j<surfaceHeights.Length ; j++)
+        for (int j=0 ; j<surfaceHeights.Length ; j++)
+        {
+            surfaceHeights[j] = 0;
+            for (int i = 0; i < osc.Length; i++)
             {
-                surfaceHeights[i] = osc[i].getValueAt(i); 
+                surfaceHeights[j] += osc[i].getValueAt((float)j * (Mathf.PI*2) / (float)surfaceHeights.Length);
             }
 	    }
     }
@@ -31,14 +34,36 @@ public class OneOsc
     public float phase = 1;
     public WaveShape shape = WaveShape.sine;
     
-    public float getValueAt(int x) {
-        return Mathf.Sin(x * frequency + phase) * amplitude;
+    public float getValueAt(float x) {
+        float phasor = (x * frequency + phase) % (Mathf.PI*2);
+        if (shape == WaveShape.sine)
+        {
+            return Mathf.Sin(phasor) * amplitude;
+        }
+        if (shape == WaveShape.triangle)
+        {
+            float value = phasor;
+            if (phasor > Mathf.PI * 1 / 2) value -= (phasor - (Mathf.PI / 2)) * 2.0f;
+            if (phasor > Mathf.PI * 3 / 2) value += (phasor - (Mathf.PI * 3 / 2)) * 2.0f;
+            return value * amplitude;
+        }
+        if (shape == WaveShape.square)
+        {
+            if (phasor < Mathf.PI) return amplitude;
+            return -amplitude;
+        }
+        if (shape == WaveShape.saw)
+        {
+            return ((phasor/(Mathf.PI*2))-0.5f) * -amplitude;
+        }
+        return 0;
     }
 
     public enum WaveShape {
         sine,
         triangle,
         square,
+        saw,
         noise
     }
     
