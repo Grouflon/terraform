9191eff20337ad520c5a737f2bd41e5c02d49a47
Remi laptop <remi@uselessworks.com>
2017-01-21 19:14:27 +0100

    simplified input. post-renderer only on terraforming

--- Assets/OscillatorController.cs
+++ Assets/OscillatorController.cs
@@ -12,7 +12,10 @@ public class OscillatorController : MonoBehaviour {
     public float minAmplitude = 1.0f;
     public float maxAmplitude = 100.0f;
 
-    public float phaseChangeFactor = 1.0f;
+    public float amplitudeSensitivity = 0.01f;
+    public float frequencySensitivity = 0.01f;
+
+    public float phaseSensitivity = 1.0f;
 
     void Start()
     {
@@ -32,26 +35,26 @@ public class OscillatorController : MonoBehaviour {
 
         OneOsc currentOscillator = oscillator.osc[m_currentWave];
 
-        if (!m_statesManager.input.IsAmplitudeLocked())
-            currentOscillator.amplitude = Mathf.Lerp(minAmplitude, maxAmplitude, m_statesManager.input.GetAmplitude());
-
-        if (!m_statesManager.input.IsFrequencyLocked())
-            currentOscillator.frequency = Mathf.Lerp(minFrequency, maxFrequency, m_statesManager.input.GetFrequency());
-
-        if (m_statesManager.input.GetSineShapeChange()) currentOscillator.shape = OneOsc.WaveShape.sine;
-        if (m_statesManager.input.GetSquareShapeChange()) currentOscillator.shape = OneOsc.WaveShape.square;
-        if (m_statesManager.input.GetSawShapeChange()) currentOscillator.shape = OneOsc.WaveShape.saw;
-        if (m_statesManager.input.GetNoiseShapeChange()) currentOscillator.shape = OneOsc.WaveShape.noise;
-
-        currentOscillator.phase += m_statesManager.input.GetPhase() * phaseChangeFactor;
-
-        if (m_statesManager.input.NextWave())
+        if (m_statesManager.input.GetAnyShapeMode())
         {
-            m_currentWave = (m_currentWave + 1) % oscillator.osc.Length;
-        }
-        if (m_statesManager.input.PreviousWave())
-        {
-            m_currentWave = (m_currentWave - 1 + oscillator.osc.Length) % oscillator.osc.Length;
+            currentOscillator.amplitude = Mathf.Clamp(currentOscillator.amplitude + m_statesManager.input.GetAmplitudeChange() * amplitudeSensitivity, minAmplitude, maxAmplitude);
+            currentOscillator.frequency = Mathf.Clamp(currentOscillator.frequency + m_statesManager.input.GetFrequencyChange() * frequencySensitivity, minFrequency, maxFrequency);
+
+            if (m_statesManager.input.GetSineShapeMode()) currentOscillator.shape = OneOsc.WaveShape.sine;
+            if (m_statesManager.input.GetSquareShapeMode()) currentOscillator.shape = OneOsc.WaveShape.square;
+            if (m_statesManager.input.GetSawShapeMode()) currentOscillator.shape = OneOsc.WaveShape.saw;
+            if (m_statesManager.input.GetNoiseShapeMode()) currentOscillator.shape = OneOsc.WaveShape.noise;
+
+            currentOscillator.phase += m_statesManager.input.GetPhaseChange() * phaseSensitivity;
+
+            if (m_statesManager.input.NextWave())
+            {
+                m_currentWave = (m_currentWave + 1) % oscillator.osc.Length;
+            }
+            if (m_statesManager.input.PreviousWave())
+            {
+                m_currentWave = (m_currentWave - 1 + oscillator.osc.Length) % oscillator.osc.Length;
+            }
         }
     }
 
