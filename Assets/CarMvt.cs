6570b032ab178c3ab4a32f347c13561ae06b78c7
Morusque <Morusque@gmail.com>
2017-01-21 17:56:34 +0100

    car states

--- Assets/CarMvt.cs
+++ Assets/CarMvt.cs
@@ -6,20 +6,43 @@ public class CarMvt : MonoBehaviour {
 
     public GameObject statesManager;
 
-	void Start () {
+    Vector2 lastRunningPosition;
+    Vector2 lastTerraformPosition;
+
+    void Start () {
 		
 	}
-	
-	void Update () {
-        this.GetComponent<Rigidbody2D>().AddForce(new Vector2(1.0f, 0));
-        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y+transform.localScale.y), Vector2.up);
-        if (statesManager.GetComponent<StatesManager>().state == StatesManager.GameStates.terraform) { 
-            if (hit.collider!=null) {
+
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + transform.localScale.y), Vector2.up);
+        
+        if (statesManager.GetComponent<StatesManager>().state == StatesManager.GameStates.terraform)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+            if (hit.collider != null)
+            {
                 Vector2 pos = transform.position;
-                pos.y += hit.distance + transform.localScale.y*2;
+                pos.y += hit.distance + transform.localScale.y * 2;
                 transform.position = pos;
             }
+            transform.position = new Vector2(lastRunningPosition.x, Mathf.Min(lastRunningPosition.y,transform.position.y));
+            lastTerraformPosition = new Vector2(transform.position.x, transform.position.y);
         }
+
+        if (statesManager.GetComponent<StatesManager>().state == StatesManager.GameStates.running)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(3.0f, 0));
+            transform.position = new Vector2(transform.position.x%200, transform.position.y);
+            if (hit.collider != null)
+            {
+                Vector2 pos = transform.position;
+                pos.y += hit.distance + transform.localScale.y * 2;
+                transform.position = pos;
+            }
+            lastRunningPosition = new Vector2(transform.position.x, transform.position.y);
+        }
+
     }
-    
+
 }
