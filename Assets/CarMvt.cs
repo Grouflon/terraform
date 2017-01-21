4b296a5803bf5b3bb53a48718c398c59e755bf23
Morusque <Morusque@gmail.com>
2017-01-21 15:42:39 +0100

    car

---
+++ Assets/CarMvt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMvt : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        this.GetComponent<Rigidbody2D>().AddForce(new Vector2(1.0f, 0));
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y+transform.localScale.y), Vector2.up);
+        if (hit.collider!=null) {
+            Vector2 pos = transform.position;
+            pos.y += hit.distance + transform.localScale.y*2;
+            transform.position = pos;
+        }
+	}
+}
