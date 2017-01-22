7d4bce2a9eb8760fbfb3ad07b11c4e92eeb4a527
Remi laptop <remi@uselessworks.com>
2017-01-22 20:21:02 +0100

    Bouncing loot

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -11,6 +11,7 @@ public class Enemy : MonoBehaviour {
 
     float swingFr = 0;
     float swingAm = 0;
+    float lifeTime = 0;
 
     void Start()
     {
@@ -20,13 +21,16 @@ public class Enemy : MonoBehaviour {
         swingFr = Random.value * 2.0f + 2.0f;
         swingAm = Random.value * 30.0f;
         m_baseScale = transform.localScale;
+        transform.localScale = new Vector3(1, 1, 1) * 0.00000001f;
     }
 
     void Update()
     {
+        lifeTime += Time.deltaTime;
+
         float t = Mathf.Sin(Time.time * swingFr);
         transform.rotation = Quaternion.Euler(0, 0, t * swingAm);
-        transform.localScale = m_baseScale + new Vector3(1.0f, 1.0f, 1.0f) * t * 0.1f;
+        transform.localScale = m_baseScale * Ease.BackOut(Mathf.Clamp01(lifeTime / 0.5f)) + new Vector3(1.0f, 1.0f, 1.0f) * t * 0.1f;
     }
 
     void OnTriggerEnter2D(Collider2D other)
--- Assets/Loot.cs
+++ Assets/Loot.cs
@@ -9,6 +9,7 @@ public class Loot : MonoBehaviour {
 
     float swingFr = 0;
     float swingAm = 0;
+    float lifeTime = 0;
 
     void Start () {
         audioManager = FindObjectOfType<AudioManager>();
@@ -17,13 +18,16 @@ public class Loot : MonoBehaviour {
         swingFr = Random.value * 2.0f + 2.0f;
         swingAm = Random.value * 30.0f;
         m_baseScale = transform.localScale;
+        transform.localScale = new Vector3(1,1,1) * 0.00000001f;
     }
 
     void Update ()
     {
+        lifeTime += Time.deltaTime;
+
         float t = Mathf.Sin(Time.time * swingFr);
         transform.rotation = Quaternion.Euler(0, 0, t * swingAm);
-        transform.localScale = m_baseScale + new Vector3(1.0f, 1.0f, 1.0f) * t * 0.1f;
+        transform.localScale = m_baseScale * Ease.BackOut(Mathf.Clamp01(lifeTime / 0.5f)) + new Vector3(1.0f, 1.0f, 1.0f) * t * 0.1f;
     }
 
     void OnTriggerEnter2D(Collider2D other)
